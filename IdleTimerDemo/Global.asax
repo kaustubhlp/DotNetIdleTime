73aaa98a06dbbd04f6487aaae45236cbb32a31a3
Kaustubh Kuber <kaustubhk@nitorinfotech.com>
2019-04-08 23:19:22 +0530

    Initial commit

---
+++ IdleTimerDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IdleTimerDemo.Global" Language="C#" %>
---
+++ IdleTimerDemo/Scripts/Common.js
@@ -0,0 +1,16 @@
+﻿var timeout = 10;
+var countdownTimer = 10;
+
+$(document).ready(function () {
+    BindTimer();
+});
+
+function BindTimer() {
+    $(document).bind("idle.idleTimer", function () {
+
+        $.timeoutDialog({ timeout: timeout, countdown: countdownTimer, logout_redirect_url: 'AutoLogout.aspx', restart_on_yes: true });
+
+    });
+
+    $.idleTimer(timeout * 1000);
+}
